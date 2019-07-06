314e8637d5f01a78082f9f8801558135831bb6a2
Stewart ilondanga <stilondanga@gmail.com>
2019-07-06 23:28:09 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,13 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+var form = document.getElementById("formAwesome");
+form.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  $('#formModal').modal('hide');
+  $('#btnStart').hide();
+  $('#message').show();
+}
